e73e40802addddfd5f9c2d602dc1cf9a113a03e4
Alicia <aliciav472@gmail.com>
2025-11-21 17:51:30 +0100

    Añadido el proyecto al repositorio y cambio de puerto a 8091.

---
+++ src/test/java/com/example/ExamenMocAVV/ExamenMocAvvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocAVV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocAvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
