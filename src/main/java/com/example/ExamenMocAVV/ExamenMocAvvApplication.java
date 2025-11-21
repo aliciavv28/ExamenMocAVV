8d85be60ed79a0804f2658b2b90e05e6385251ec
Alicia <aliciav472@gmail.com>
2025-11-21 17:37:50 +0100

    Proyecto inicial

---
+++ src/main/java/com/example/ExamenMocAVV/ExamenMocAvvApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocAVV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocAvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocAvvApplication.class, args);
+	}
+
+}
